74181d65dd62a94c3a3c104ba00ea78d9ed6b197
kwontwitch <hancom114@gmail.com>
2019-07-13 08:46:26 +0900

    문제 제기 Connection 이벤트는 포트 4000 터미널에서 감지된다. 그런데 Connection 이벤트의 내부 함수 initSocketConnect의 console.log는 어째서 양 포트 터미널 모두에 표시되는가?

--- src/p2p.js
+++ src/p2p.js
@@ -8,6 +8,12 @@ const startP2PServer = server => {
   const wsServer = new WebSockets.Server({ server });
   wsServer.on("connection", ws => {
     initSocketConnection(ws);
+    console.log('connection 이벤트 발생. initSocketConnection 실행됨.');
+// 포트 3000에게 포트 4000으로 연결하라는 포스트를 보낼 경우,
+// 위 로그가 포트 4000에만 표시된다.
+// 그런데 어째서 같은 선상의 함수 initSocketConnection이 가지고 있는 로그 welcome은
+// 양쪽 포트의 터미널 모두에 표시되는 걸까? - 문제 (1 of 1)
+
   });
   console.log("Nomadcoin P2P Server running");
 };
