c234ccfb5912466ed5e3daecc81551207bcca0b2
kwontwitch <hancom114@gmail.com>
2019-07-13 11:08:52 +0900

    모듈 ws를 통해 new ws가 생성되는 과정 관찰 /peers path를 통해 POST를 받으면, req.body.peer의 값을 참조하여 ex) req.body.peer ==  ws://localhost:4000 connectToPeers 함수를 호출한다. 이 함수는 받은 peer값을 받아 ws 모듈 기반의 객체 ws를 생성한다. (new) 생성한 다음 open 이벤트를 on한다. ws를 initSocketConnection으로 넘겨 sockets 배열에 넣도록 한다. 다음 과정은 생략.

--- src/p2p.js
+++ src/p2p.js
@@ -23,8 +23,17 @@ const initSocketConnection = socket => {
 };
 
 const connectToPeers = newPeer => {
+  // 포트 3000에 포트 4000으로 연결하라는 포스트를 보내면
+  // newPeer는 ws://localhost:4000이라는 값을 할당받는다.
   const ws = new WebSockets(newPeer);
+  // 포트 4000에 해당하는 정보로 웹소켓이 생성된다.
+  // !! 주의할점은 위 ws의 주체는 포트 3000이다.
+  // !! 이는 포트 3000에게 포트 4000으로 연결하라는 포스트를 보냈을 때,
+  // !! open 이벤트(아래 ws.on의 이벤트)가 포트 3000에만 일어나는 걸 통해 알 수 있다. (로그로 확인.)
+  // !! 웹소켓이 포트 4000에 대한 정보를 받아 만들어 졌지만
+  // !! ws가 3000을 토대로 만들어 졌음을 짐작할 수 있다.
   ws.on("open", () => {
+    console.log('open 이벤트는 언제 발생될까요오 ~?');
     initSocketConnection(ws);
   });
 };
